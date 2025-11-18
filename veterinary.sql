1a1e674f49971344294bca62d3baca5b504a089c
baeverly <baeverlycabillan2gmail.com>
2025-11-18 23:22:05 +0800

    feat/insert-10-rows-owners

--- veterinary.sql
+++ veterinary.sql
@@ -63,3 +63,16 @@ CREATE TABLE medicalrecords
     FOREIGN KEY (animalid) references animals(animalid),
     FOREIGN KEY (doctorid) references doctors(doctorid)
 );
+
+INSERT INTO owners (ownerid, ofirstname, olastname, address, phone, email)
+VALUES 
+(1, 'Mark', 'Santos', 'Manila', '09101112222', 'mark.santos@example.com'),
+(2, 'Liza', 'Reyes', 'Quezon City', '09223334455', 'liza.reyes@example.com'),
+(3, 'Carlo', 'Garcia', 'Tagaytay', '09335556677', 'carlo.garcia@example.com'),
+(4, 'Mika', 'Torres', 'Batangas', '09447778899', 'mika.torres@example.com'),
+(5, 'Evan', 'Lopez', 'Laguna', '09556667788', 'evan.lopez@example.com'),
+(6, 'Rhea', 'Cruz', 'Cebu City', '09667778800', 'rhea.cruz@example.com'),
+(7, 'Daniel', 'Rivera', 'Pasig', '09778889911', 'daniel.rivera@example.com'),
+(8, 'Sofia', 'Mendoza', 'Makati', '09889990022', 'sofia.mendoza@example.com'),
+(9, 'Kevin', 'Villanueva', 'Baguio', '09990001133', 'kevin.villanueva@example.com'),
+(10, 'Alyssa', 'Navarro', 'Davao City', '09123450099', 'alyssa.navarro@example.com');
