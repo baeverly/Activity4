b00947e37e080d44fbcbef44fdf4f53c14911959
jhonrheytenorio-ui <jhonrheytenorio@gmail.com>
2025-11-18 23:41:53 +0800

    feat/insert-10-rows-medicalrecords

--- veterinary.sql
+++ veterinary.sql
@@ -116,15 +116,30 @@ VALUES
     (5, 'Dr.Luis', 'Torres', 'Surgery Specialist', '123-555-7777', 'luis@example.com'),
     (6, 'Dr.Carmen', 'Fernandez', 'Opthalmology Specialist', '333-222-1111', 'carmen@example.com');
 
-INSERT INTO invoices(invoiceid, appointid, totalamount, paymentdate)
+INSERT INTO invoices
+    (invoiceid, appointid, totalamount, paymentdate)
 VALUES
-(1, 1, 50.00, '09:30:00'),
-(2, 2, 75.00, '14:15:00'),
-(3, 3, 100.00, '11:00:00'),
-(4, 4, 200.00, '13:45:00'),
-(5, 5, 80.00, '10:30:00'),
-(6, 6, 30.00, '15:00:00'),
-(7, 7, 75.00, '09:15:00'),
-(8, 8, 150.00, '16:30:00'),
-(9, 9, 60.00, '14:45:00'),
-(10, 10, 40.00, '11:30:00');
+    (1, 1, 50.00, '09:30:00'),
+    (2, 2, 75.00, '14:15:00'),
+    (3, 3, 100.00, '11:00:00'),
+    (4, 4, 200.00, '13:45:00'),
+    (5, 5, 80.00, '10:30:00'),
+    (6, 6, 30.00, '15:00:00'),
+    (7, 7, 75.00, '09:15:00'),
+    (8, 8, 150.00, '16:30:00'),
+    (9, 9, 60.00, '14:45:00'),
+    (10, 10, 40.00, '11:30:00');
+
+INSERT INTO medicalrecords
+    (recordid, animalid, recorddate, doctorid, diagnosis, prescription, notes)
+VALUES
+    (1, 1, '2023-01-05 00:00:00', 1, 'Health check', 'N/A', 'Regular checkup, no issue detected'),
+    (2, 2, '2023-01-10 00:00:00', 1, 'Vaccination', 'Vaccine X', 'Administered vaccination X as per schedule'),
+    (3, 3, '2023-02-02 00:00:00', 3, 'Sprained leg', 'Pain Medication', 'Rest recommended for two weeks'),
+    (4, 4, '2023-02-15 00:00:00', 1, 'Dental cleaning', 'N/A', 'Completed dental cleaning procedure'),
+    (5, 5, '2023-03-10 00:00:00', 4, 'Skin infection', 'Antibiotics', 'Prescribed antibiotics for skin infection'),
+    (6, 6, '2023-03-10 00:00:00', 2, 'Flea infestation', 'Flea Treatment', 'Administered flea treatment'),
+    (7, 7, '2023-04-12 00:00:00', 1, 'Vaccination', 'Vaccine Y', 'Administered vaccination Y as per schedule'),
+    (8, 8, '2023-04-18 00:00:00', 5, 'Spaying', 'N/A', 'Successfully performed spaying surgery'),
+    (9, 9, '2023-05-02 00:00:00', 4, 'Allergic reaction', 'Antihistamines', 'Allergic reaction due to food prescribed antihistamine'),
+    (10, 10, '2023-05-20 00:00:00', 6, 'Conjunctivitis', 'Eye drops', 'Prescribed eye drops for conjunctivitis');
